f038a42d2c9ba136978235fa05dc17defb6a425e
ardans <ardans@foxmail.com>
2024-10-08 16:13:31 +0800

    fix(vite): support vue sfc

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -1,33 +1,15 @@
 import type { Plugin } from 'vite'
-import { transformDeepSelector } from '@vue3-deep-selector-adapter/core'
+import { transformDeepSelector, transformVueSfc } from '@vue3-deep-selector-adapter/core'
 
-interface VueDeepSelectorPluginOptions {
-  include?: string | RegExp | (string | RegExp)[]
-  exclude?: string | RegExp | (string | RegExp)[]
-}
-
-export default function vueDeepSelectorPlugin(options: VueDeepSelectorPluginOptions = {}): Plugin {
-  const { include = /\.(vue|css|scss|less|styl)$/, exclude } = options
-
+export default function vueDeepSelectorAdapter(): Plugin {
   return {
     name: 'vue3-deep-selector-adapter',
-    enforce: 'pre',
     transform(code, id) {
-      if (
-        (typeof include === 'string' && !id.includes(include))
-        || (include instanceof RegExp && !include.test(id))
-        || (Array.isArray(include) && !include.some(i => (typeof i === 'string' ? id.includes(i) : i.test(id))))
-        || (exclude && (
-          (typeof exclude === 'string' && id.includes(exclude))
-          || (exclude instanceof RegExp && exclude.test(id))
-          || (Array.isArray(exclude) && exclude.some(e => (typeof e === 'string' ? id.includes(e) : e.test(id))))
-        ))
-      ) {
-        return null
+      if (id.endsWith('.vue')) {
+        return transformVueSfc(code)
       }
-      return {
-        code: transformDeepSelector(code),
-        map: null,
+      if (/\.(?:css|less|sass|scss|styl)$/.test(id)) {
+        return transformDeepSelector(code)
       }
     },
   }
